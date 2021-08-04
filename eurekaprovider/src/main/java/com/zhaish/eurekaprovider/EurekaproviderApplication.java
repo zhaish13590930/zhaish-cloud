e0a3f258a626b6c58f79aa06352ae4c97e671319
zhaishenghui <13590930@qq.com>
2021-08-04 16:09:17 +0800

    123

---
+++ eurekaprovider/src/main/java/com/zhaish/eurekaprovider/EurekaproviderApplication.java
@@ -0,0 +1,13 @@
+package com.zhaish.eurekaprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaproviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaproviderApplication.class, args);
+    }
+
+}
---
+++ eurekaprovider/src/test/java/com/zhaish/eurekaprovider/EurekaproviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaish.eurekaprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaproviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
